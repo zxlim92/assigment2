31759c42885c36f9aa336a096393d37b1a0955c7
minh7a6 <minhdangle7a6@gmail.com>
2020-03-15 09:16:53 -0600

    completed dijkstra

--- Makefile
+++ Makefile
@@ -1,17 +1,15 @@
-CC=g++
-LFLAGS=
-CFLAGS=-c -Wall -O2
-OBJS= dijkstra.o server.o digraph.o
-PROGS=server
+CC = g++
+LFLAGS = 
+CFLAGS =-c -Wall -O2
+OBJS = dijkstra.o server.o digraph.o
+PROGS = server
 header = wdigraph.h digraph.h dijkstra.h
-build = ./build
 all: $(PROGS)
-server: server.o digraph.o
-	$(CC) server.o digraph.o -o $(build)/server $(LFLAGS)
-# dijkstra.o: dijkstra.cpp
-# 	$(CC) dijkstra.cpp -o dijkstra.o $(CFLAGS)
-
-digraph.o: digraph.cpp
+server: server.o digraph.o dijkstra.o
+	$(CC) server.o digraph.o dijkstra.o -o server $(LFLAGS)
+dijkstra.o: dijkstra.cpp $(header)
+	$(CC) dijkstra.cpp -o dijkstra.o $(CFLAGS)
+digraph.o: digraph.cpp $(header)
 	$(CC) digraph.cpp -o digraph.o $(CFLAGS)
 server.o: server.cpp $(header)
 	$(CC) server.cpp -o server.o $(CFLAGS)
--- dijkstra.cpp
+++ dijkstra.cpp
@@ -4,10 +4,28 @@
 // pair is a simple container of two heterogeneous objects
 // which can be accessed by dot operator followed by 
 // first or second keyword
-
-#include <unordered_map>
-#include "wdigraph.h"
+#include "heap.h"
+#include "dijkstra.h"
 void dijkstra(const WDigraph& graph, int startVertex, 
     unordered_map<int, PIL>& searchTree) {
-	
+    searchTree = {};
+    BinaryHeap<pair<int, int> ,long long> events;
+    pair<int, int> start(startVertex, startVertex);
+    events.insert(start, 0);
+    while(events.size() > 0) {
+        auto temp = events.min();   
+        pair<int, int> edge = temp.item;
+        long long weight = temp.key;
+        int u = edge.first;
+        int v = edge.second;
+        events.popMin();
+        if(searchTree.find(v) == searchTree.end()) {
+            searchTree[v] = make_pair(weight, u);
+            for (auto i = graph.neighbours(v); i != graph.endIterator(v); i++) {
+                pair<int, int> temp(v, *i);
+                long long cost = graph.getCost(v, *i);
+                events.insert(temp, weight + cost);
+            }
+        }
+    }
 }
